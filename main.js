88c9a40c60548342620c81e76d55f1e6a6cc0968
Eiman Butt <163169388+EimanButt07@users.noreply.github.com>
2024-05-12 15:32:00 +0500

    Mothers_Day2024

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function printMothersDayMessage() {
+    console.log("Happy Mother's Day to all the amazing moms out there! Your love makes the world a brighter place. 💐");
+}
+printMothersDayMessage();
---
+++ main.ts
@@ -0,0 +1,5 @@
+function printMothersDayMessage() {
+    console.log("Happy Mother's Day to all the amazing moms out there! Your love makes the world a brighter place. 💐");
+}
+
+printMothersDayMessage();
